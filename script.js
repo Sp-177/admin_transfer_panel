fd15ae9881aecab2545def80cad1037f66a3858c
Sp-177 <shub404.x@gmail.com>
2024-06-09 23:51:01 +0530

    9-6-24

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
 
-function transfer(f,a){
-const from_sec=document.getElementById("from-list");
-const to_sec=document.getElementById("to-list");
-const selected_from=document.getElementsByClassName("check");
-const selected_to=document.getElementsByClassName("check");
-if(f==='f'){
-        for(var i=0;i<selected_from.length;i++){
-            if(from_sec.contains(selected_from[i])&&(selected_from[i].checked || a==='a' )){
-                selected_from[i].checked=false;
-                const sec=from_sec.children[i];
-                to_sec.appendChild(sec);
-                
-            }
-        }
-
+function transfer(src,dst,a){
+    const s=document.getElementById(src);
+    const d=document.getElementById(dst);
+    const source=Array.from((a==='a')?s.options:s.selectedOptions);
+    for(let i=0;i<source.length;i++){
+        d.appendChild(source[i]);
     }
-else{
-        
-        for(var i=0;i<selected_to.length;i++){
-            if(from_sec.contains(selected_to[i])&&(selected_to[i].checked || a==='a' )){
-                const sec=document.getElementById(selected_to[i].value);
-                selected_to[i].checked=false;
-                from_sec.appendChild(sec);
-                
-            }
-        }
-    }
-
+}
+function search(){
+    
 }
